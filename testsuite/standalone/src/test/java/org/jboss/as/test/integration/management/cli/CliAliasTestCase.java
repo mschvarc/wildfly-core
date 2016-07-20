b73b65cbe2887c6beb85a57d4cac10d2431c4d31
mschvarc <mschvarc@redhat.com>
2016-07-20 13:57:47 +0200

    Work on several JBEAP features

---
+++ testsuite/standalone/src/test/java/org/jboss/as/test/integration/management/cli/CliAliasTestCase.java
@@ -0,0 +1,7 @@
+package org.jboss.as.test.integration.management.cli;
+
+/**
+ * Created by mschvarc on 18/07/16.
+ */
+public class CliAliasTestCase {
+}
---
+++ testsuite/standalone/src/test/java/org/jboss/as/test/integration/management/cli/ControllerAliasConnectTestCase.java
@@ -0,0 +1,7 @@
+package org.jboss.as.test.integration.management.cli;
+
+/**
+ * Created by mschvarc on 20/07/16.
+ */
+public class ControllerAliasConnectTestCase {
+}
