dd19eb5ce693d7c71f849513aeb0b53c74904325
mschvarc <mschvarc@redhat.com>
2016-07-21 14:14:48 +0200

    jenkins testing

--- testsuite/standalone/src/test/java/org/jboss/as/test/integration/management/cli/ControllerAliasConnectTestCase.java
+++ testsuite/standalone/src/test/java/org/jboss/as/test/integration/management/cli/ControllerAliasConnectTestCase.java
@@ -11,10 +11,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 import org.jboss.as.test.shared.TestSuiteEnvironment;
 import org.junit.AfterClass;
@@ -23,16 +20,17 @@ import org.junit.Test;
 
 /**
  * @author Martin Schvarcbacher
- *
- * https://issues.jboss.org/browse/JBEAP-5009 //limitations [a-zA-Z0-9_]+ only var names
- * https://issues.jboss.org/browse/JBEAP-3164
+ *         <p>
+ *         https://issues.jboss.org/browse/JBEAP-5009 //limitations [a-zA-Z0-9_]+ only var names
+ *         https://issues.jboss.org/browse/JBEAP-3164
  */
 @RunWith(WildflyTestRunner.class)
 public class ControllerAliasConnectTestCase {
 
     private static final File TMP_JBOSS_CLI_FILE;
-    private static final String SERVER_ALIAS = "TestSuiteServer";
-    private static final int INVALID_PORT = TestSuiteEnvironment.getServerPort() - 1;
+    private static final String SERVER_ALIAS = "Test-Suite_Server-Name";
+    private static final String SERVER_ALIAS_INVALID = "Test1#-$Suite2_*Server3-Invalid4\\-Name5";
+    private static final int INVALID_PORT = TestSuiteEnvironment.getServerPort() - 2;
 
     static {
         TMP_JBOSS_CLI_FILE = new File(new File(TestSuiteEnvironment.getTmpDir()), "tmp-jboss-cli.xml");
@@ -41,39 +39,31 @@ public class ControllerAliasConnectTestCase {
     @BeforeClass
     public static void setup() {
         ensureRemoved(TMP_JBOSS_CLI_FILE);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(TMP_JBOSS_CLI_FILE))) {
-            writer.write("<?xml version='1.0' encoding='UTF-8'?>");
-            writer.newLine();
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(TMP_JBOSS_CLI_FILE), "UTF-8"))) {
+            writer.write("<?xml version='1.0' encoding='UTF-8'?>\n");
             writer.write("<jboss-cli xmlns=\"urn:jboss:cli:2.0\">\n");
-            writer.newLine();
-            writer.write("<default-protocol  use-legacy-override=\"true\">http-remoting</default-protocol> ");
-            writer.newLine();
-            writer.write("<default-controller>  ");
-            writer.newLine();
-            writer.write("<protocol>http-remoting</protocol>");
-            writer.newLine();
-            writer.write("<host>localhost</host>");
-            writer.newLine();
-            writer.write("<port>" + INVALID_PORT + "</port>");
-            writer.newLine();
-            writer.write("</default-controller> ");
-            writer.write("<controllers>");
-            writer.newLine();
-            writer.write("<controller name=\"" + SERVER_ALIAS + "\">");
-            writer.newLine();
-            writer.write("<protocol>http-remoting</protocol>");
-            writer.newLine();
-            writer.write("<host>" + TestSuiteEnvironment.getServerAddress() + "</host>");
-            writer.newLine();
-            writer.write("<port>" + TestSuiteEnvironment.getServerPort() + "</port>");
-            writer.newLine();
-            writer.write("</controller>");
-            writer.newLine();
-            writer.write("</controllers>");
-            writer.newLine();
-            writer.write("</jboss-cli>");
-            writer.newLine();
+            writer.write("<default-protocol  use-legacy-override=\"true\">http-remoting</default-protocol>\n");
+            writer.write("<default-controller>\n");
+            writer.write("<protocol>http-remoting</protocol>\n");
+            writer.write("<host>localhost</host>\n");
+            writer.write("<port>" + INVALID_PORT + "</port>\n");
+            writer.write("</default-controller>\n");
+            writer.write("<controllers>\n");
 
+            writer.write("<controller name=\"" + SERVER_ALIAS + "\">\n");
+            writer.write("<protocol>http-remoting</protocol>\n");
+            writer.write("<host>" + TestSuiteEnvironment.getServerAddress() + "</host>\n");
+            writer.write("<port>" + TestSuiteEnvironment.getServerPort() + "</port>\n");
+            writer.write("</controller>\n");
+
+            writer.write("<controller name=\"" + SERVER_ALIAS_INVALID + "\">\n");
+            writer.write("<protocol>http-remoting</protocol>\n");
+            writer.write("<host>" + TestSuiteEnvironment.getServerAddress() + "</host>\n");
+            writer.write("<port>" + TestSuiteEnvironment.getServerPort() + "</port>\n");
+            writer.write("</controller>\n");
+
+            writer.write("</controllers>\n");
+            writer.write("</jboss-cli>\n");
         } catch (IOException e) {
             fail(e.getLocalizedMessage());
         }
@@ -128,6 +118,25 @@ public class ControllerAliasConnectTestCase {
         cli.executeNonInteractive();
         assertServerConnected(cli.getOutput());
     }
+
+    /**
+     * Tests connection to a controller aliased in jboss-cli.xml
+     * using controller=MyController
+     */
+    @Test
+    public void testConnectToInvalidAliasedController() throws Exception {
+        CliProcessWrapper cli = new CliProcessWrapper()
+                .addCliArgument("-Djboss.cli.config=" + TMP_JBOSS_CLI_FILE.toPath())
+                .addCliArgument("--controller=" + SERVER_ALIAS_INVALID)
+                .addCliArgument("--connect")
+                .addCliArgument("--echo-command")
+                .addCliArgument("--commands=:read-attribute(name=server-state)");
+        cli.executeNonInteractive();
+        String output = cli.getOutput();
+        output.toString();
+        assertServerConnected(cli.getOutput());
+    }
+
 
     private void assertServerConnected(String output) {
         String expected = "@" + TestSuiteEnvironment.getServerAddress() + ":" + TestSuiteEnvironment.getServerPort();
